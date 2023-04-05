b96acfb0b58ae30300d926791342c9762bfe3a3e
Marek Jacob <MeraX@users.noreply.github.com>
2023-04-05 09:27:24 +0200

    Add CSV to test and update netcdf stats test such that it uses create_stats_dataframe correctly

--- tests/engine/test_stats.py
+++ tests/engine/test_stats.py
@@ -6,8 +6,6 @@ from netCDF4 import Dataset
 
 from engine.stats import create_stats_dataframe
 
-DUMMY_FILE_NAME = "test_stats.nc"
-
 TIME_DIM_SIZE = 3
 HOR_DIM_SIZE = 100
 HEIGHT_DIM_SIZE = 5
@@ -31,9 +29,12 @@ def initialize_dummy_netcdf_file(name):
     return data
 
 
-class TestStats(unittest.TestCase):
+class TestStatsNetcdf(unittest.TestCase):
+    nc_file_name = "test_stats.nc"
+    stats_file_names = "test_stats.csv"
+
     def setUp(self):
-        data = initialize_dummy_netcdf_file(DUMMY_FILE_NAME)
+        data = initialize_dummy_netcdf_file(self.nc_file_name)
 
         data.createVariable("v1", np.float64, dimensions=("t", "z", "x"))
         data.variables["v1"][:] = np.ones(
@@ -55,22 +56,34 @@ class TestStats(unittest.TestCase):
         data.close()
 
     def TearDown(self):
-        os.remove(DUMMY_FILE_NAME)
-        os.remove(DUMMY_FILE_NAME.replace(".nc", ".csv"))
+        os.remove(self.nc_file_name)
+        os.remove(self.stats_file_names)
 
     def test_stats(self):
-        horizontal_dim = "x"
+        file_specification = [
+            [
+                "Test data",
+                "*.nc",
+                dict(format="netcdf", time_dim="t", horizontal_dims=["x"]),
+            ],
+            [
+                # The file_ids glob matches to many files, so we use the "ignore"
+                # format to ignore them
+                "other files",
+                "*",
+                dict(format="ignore"),
+            ],
+        ]
 
         df = create_stats_dataframe(
             input_dir=".",
-            file_ids=[DUMMY_FILE_NAME.replace(".nc", "")],
-            time_dim="t",
-            horizontal_dims=[horizontal_dim],
-            stats_file_name=DUMMY_FILE_NAME.replace(".nc", ".csv"),
+            file_ids=[self.nc_file_name.replace(".nc", "")],
+            stats_file_name=self.stats_file_names,
+            file_specification=file_specification,
         )
 
-        # check that the min/mean/max are correct
-        sol = np.array(
+        # check that the mean/max/min are correct
+        expected = np.array(
             [
                 [1.0, 2.0, 0.0, 1.0, 2.0, 0.0, 1.0, 2.0, 0.0],
                 [1.0, 2.0, 0.0, 1.0, 2.0, 0.0, 1.0, 2.0, 0.0],
@@ -83,8 +96,66 @@ class TestStats(unittest.TestCase):
         )
 
         self.assertTrue(
-            np.array_equal(df.values, sol),
-            "stats dataframe incorrect. Difference:\n{}".format(df.values == sol),
+            np.array_equal(df.values, expected),
+            "stats dataframe incorrect. Difference:\n{}".format(df.values == expected),
+        )
+
+
+class TestStatsCsv(unittest.TestCase):
+    dat_file_name = "test_stats_csv.dat"
+    stats_file_name = "test_stats_csv.csv"
+
+    def setUp(self):
+        lines = (
+            "time v1  v2 v3 v4 v5",
+            "10   1.4 15 16 17 18",
+            "20   2.4 25 26 27 28",
+            "30   3.4 35 36 37 38",
+        )
+        with open(self.dat_file_name, "w") as f:
+            f.write("\n".join(lines))
+
+    def TearDown(self):
+        os.remove(self.dat_file_name)
+        os.remove(self.stats_file_name)
+
+    def test_stats(self):
+        file_specification = [
+            [
+                "Test data",
+                "*csv.dat",
+                dict(
+                    format="csv",
+                    parser_args=dict(
+                        delimiter="\\s+",
+                        header=0,
+                        index_col=0,
+                    ),
+                ),
+            ],
+        ]
+
+        df = create_stats_dataframe(
+            input_dir=".",
+            file_ids=[self.dat_file_name],
+            stats_file_name=self.stats_file_name,
+            file_specification=file_specification,
+        )
+
+        # check that the mean/max/min are correct (i.e. the same as in CSV)
+        expected = np.array(
+            [
+                [1.4, 1.4, 1.4, 2.4, 2.4, 2.4, 3.4, 3.4, 3.4],
+                [15, 15, 15, 25, 25, 25, 35, 35, 35],
+                [16, 16, 16, 26, 26, 26, 36, 36, 36],
+                [17, 17, 17, 27, 27, 27, 37, 37, 37],
+                [18, 18, 18, 28, 28, 28, 38, 38, 38],
+            ],
+        )
+
+        self.assertTrue(
+            np.array_equal(df.values, expected),
+            "stats dataframe incorrect. Difference:\n{}".format(df.values == expected),
         )
 
 
