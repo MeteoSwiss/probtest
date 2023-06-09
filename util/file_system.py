a14b92e17604abf608a6ebd3f303266747f3c2b5
Marek Jacob <MeraX@users.noreply.github.com>
2023-06-09 14:28:29 +0200

    allow for sub directories in file name pattern.

--- util/file_system.py
+++ util/file_system.py
@@ -4,8 +4,26 @@ from util.log_handler import logger
 
 
 def file_names_from_pattern(dir_name, file_pattern):
+    """
+    Search for all file patching file_pattern in directory dir_name
+
+    Parameters
+    ----------
+    dir_name : str
+    file_pattern: str
+        file_pattern may contain simple shell-style wildcards such as "*".
+        file_pattern may contain "/" to specify subdirectories.
+
+    Returns
+    -------
+    file_names: list of str
+        A list of matched file names that are relative to dir_name.
+    """
     err = 0
-    file_names = [p.name for p in Path(dir_name).glob(file_pattern)]
+    matched_pattern = Path(dir_name).glob(file_pattern)
+    # use .relative_to to allow file_pattern to contain subdirectories.
+    file_names = [p.relative_to(dir_name) for p in matched_pattern]
+    file_names = [str(f) for f in file_names]
     if len(file_names) < 1:
         logger.warning(
             "no files found in '{}' for pattern '{}'".format(dir_name, file_pattern)
