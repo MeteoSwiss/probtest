0b686da935def6db8ff178f0d63b7e1858bf6ef3
Marek Jacob <MeraX@users.noreply.github.com>
2023-08-24 17:13:18 +0200

    make code more explicit.

--- util/dataframe_ops.py
+++ util/dataframe_ops.py
@@ -26,7 +26,7 @@ def compute_rel_diff_dataframe(df1, df2):
     out = (df1 - df2) / average
     out = out.abs()
     # put 0 if both numbers are very small
-    zeros = (df1.abs() < CHECK_THRESHOLD) & (df2.abs() < CHECK_THRESHOLD)
+    zeros = np.logical_and(df1.abs() < CHECK_THRESHOLD, df2.abs() < CHECK_THRESHOLD)
     out[zeros] = 0.0
     return out
 
