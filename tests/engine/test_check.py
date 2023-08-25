0f22ecd9e6cf02a341dbd5b826c626028b5512e1
Marek Jacob <MeraX@users.noreply.github.com>
2023-08-25 16:08:53 +0200

    Make computation of relative difference symmetrical
    These changes also handle the case of reference values close to 0
    better.
    
    Before, if df1, the reference value, was very small, the relative
    difference was automatically set to 0 regardless the value of df2.
    
    As a result of this commit, we need new tolerances for all experiments

--- tests/engine/test_check.py
+++ tests/engine/test_check.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 
 from engine.check import check_variable
+from util.constants import CHECK_THRESHOLD
 from util.dataframe_ops import compute_rel_diff_dataframe
 
 
@@ -17,21 +18,26 @@ class TestCheck(unittest.TestCase):
             [range(4), ["max", "min", "mean"]], names=["time", "statistic"]
         )
 
-        self.df1 = pd.DataFrame(np.random.rand(4, 12) + 1, index=index, columns=columns)
-        self.df2 = pd.DataFrame(np.random.rand(4, 12) + 2, index=index, columns=columns)
+        self.df1 = pd.DataFrame(
+            np.linspace(0.9, 1.9, 4 * 12).reshape(4, 12), index=index, columns=columns
+        )
+        self.df2 = pd.DataFrame(
+            np.linspace(1.1, 2.1, 4 * 12).T.reshape(4, 12), index=index, columns=columns
+        )
+        self.df1[:2] *= -1  # some negative test data
+        self.df2[:2] *= -1
+        # Relative differences (df1-df2)/((df1+df2)/2) are between 0.2 and 0.1.
+
         self.tol1 = pd.DataFrame(
-            np.ones((2, 12)) * 3, index=["var_1", "var_2"], columns=columns
+            np.ones((2, 12)) * 0.21, index=["var_1", "var_2"], columns=columns
         )
         self.tol2 = pd.DataFrame(
-            np.ones((2, 12)), index=["var_1", "var_2"], columns=columns
+            np.ones((2, 12)) * 0.15, index=["var_1", "var_2"], columns=columns
         )
 
-    def tearDown(self):
-        return
-
-    def test_check(self):
+    def check(self, df1, df2):
         # compute relative difference
-        diff_df = compute_rel_diff_dataframe(self.df1, self.df2)
+        diff_df = compute_rel_diff_dataframe(df1, df2)
         # take maximum over height
         diff_df = diff_df.groupby(["variable"]).max()
 
@@ -48,27 +54,47 @@ class TestCheck(unittest.TestCase):
             + "Here is the DataFrame:\n{}".format(err2),
         )
 
-        return
+    def test_check(self):
+        self.check(self.df1, self.df2)
 
-    def test_check_zeros(self):
-        self.df1.loc["var_1", (2, "max")] = 0
+    def test_check_one_zero(self):
+        """Probtest should not pass if any of the values is 0
+        and the other is much larger"""
+        df1 = self.df1.copy()
+        df1.loc["var_1", (2, "max")] = 0
+        df2 = self.df2.copy()
+        diff_df = compute_rel_diff_dataframe(df1, df2)
+        diff_df = diff_df.groupby(["variable"]).max()
 
-        self.test_check()
-        return
+        out, err, _ = check_variable(diff_df, self.tol1)
 
-    def test_check_smalls1(self):
-        self.df1.loc["var_1", (2, "max")] = 1e-25
-        self.df2.loc["var_1", (2, "max")] = 1e-14
+        self.assertFalse(
+            out,
+            "Check with 0-value reference validated! "
+            + "Here is the DataFrame:\n{}".format(err),
+        )
 
-        self.test_check()
-        return
+        df2.loc["var_1", (2, "max")] = CHECK_THRESHOLD / 2
+        # now, both data are comparable again
+        self.check(df1, df2)
 
-    def test_check_smalls2(self):
-        self.df1.loc["var_1", (2, "max")] = 1e-14
-        self.df2.loc["var_1", (2, "max")] = 1e-25
+    def test_check_smalls(self):
+        """both values are close to 0 and should be accepted even though
+        their relative difference is large.
 
-        self.test_check()
-        return
+        Close to 0 means < util.constants.CHECK_THRESHOLD"""
+        df1 = self.df1.copy()
+        df1.loc["var_1", (2, "min")] = CHECK_THRESHOLD * 1e-5
+        df2 = self.df2.copy()
+        df2.loc["var_1", (2, "min")] = CHECK_THRESHOLD / -2
+        self.check(df1, df2)
+
+
+class TestCheckSwapped(TestCheck):
+    """Test that all Checks are symmetrical"""
+
+    def check(self, df1, df2):
+        super().check(df2, df1)
 
 
 if __name__ == "__main__":
