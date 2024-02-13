159eb12cbfbb3725761e4fce0d40648b25aaed57
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2024-02-13 15:31:00 +0100

    Add function to plot rankings across members

--- engine/optimal_member_sel.py
+++ engine/optimal_member_sel.py
@@ -12,7 +12,42 @@ from util.dataframe_ops import (
     parse_probtest_csv,
 )
 from util.log_handler import logger
+import matplotlib.pyplot as plt
+import numpy as np
 from scipy.stats import rankdata
+
+
+def plot_rankings(rankings, variables, total_member_num):
+
+    # Initialize a dictionary to store the frequency of rankings for each entry in each variable
+    ranking_frequency = {index: [0 for _ in range(total_member_num)] for index in range(total_member_num)}
+    # Count the frequency of each ranking for each entry in each variable
+    for variable, ranking in rankings.items():
+        for index, rank in enumerate(ranking):
+            ranking_frequency[index][rank-1] = ranking_frequency[index][rank-1] + 1
+
+    # Restructure for plotting
+    rf = []
+    for key, val in ranking_frequency.items():
+        rf.append(val)
+    rf = np.array(rf)
+    ranks = list(map(str,ranking_frequency.keys()))
+
+    colors = plt.cm.viridis(np.linspace(0, 1, total_member_num))
+    for i in range(total_member_num):
+        if i==0:
+            plt.bar(range(total_member_num), rf[:,i], label=f'Rank {i+1}',color=colors[i])
+        else:
+            plt.bar(range(total_member_num), rf[:,i], bottom=np.sum(rf[:,0:i],axis=1), label=f'Rank {i+1}',color=colors[i])
+    # Add labels and title
+    plt.xticks(range(total_member_num), ranks)
+    plt.xlabel('Ensemble Member')
+    plt.ylabel('Frequency')
+    plt.title('Frequency of Rankings')
+    plt.legend(title='Rankings', bbox_to_anchor=(1.05, 1), loc='upper left')
+    plt.tight_layout()
+    print("Saving figure to ranking_frequency_stacked_bar_entries.png")
+    plt.savefig('ranking_frequency_stacked_bar_entries.png', dpi=300)
 
 
 @click.command()
@@ -42,7 +77,14 @@ from scipy.stats import rankdata
     default=100,
     help=cli_help["total_member_num"],
 )
-def optimal_member_sel(stats_file_name, tolerance_file_name, member_num, member_type,total_member_num):
+@click.option(
+    "--plot-rank-dist/--no-plot-rank-dist",
+    is_flag=True,
+    help=cli_help["plot_rank_dist"],
+)
+
+
+def optimal_member_sel(stats_file_name, tolerance_file_name, member_num, member_type, total_member_num, plot_rank_dist):
     if len(member_num) == 1:
         member_num = [i for i in range(1, member_num[0] + 1)]
     # read in stats files
@@ -64,7 +106,7 @@ def optimal_member_sel(stats_file_name, tolerance_file_name, member_num, member_
     vars=set(index[1] for index in df_ref.index)
 
     # Initialize a dictionary to store maximum values for each variable
-    max_values_per_var = {var: [] for var in vars}
+    max_values = {var: [] for var in vars}
 
     # Iterate through each df_diff
     for i in range(total_member_num):
@@ -75,16 +117,20 @@ def optimal_member_sel(stats_file_name, tolerance_file_name, member_num, member_
             # Extract maximum value for the current variable in the current df_diff
             max_value = df_diff.loc[(slice(None), var), :].max().max()
             # Append the maximum value to the array corresponding to the variable
-            max_values_per_var[var].append(max_value)
+            max_values[var].append(max_value)
 
     # Initialize a dictionary to store rankings for each variable
-    rankings_per_var = {}
+    ranking = {}
 
     # Iterate through each variable
-    for var, max_values in max_values_per_var.items():
+    # Some indices may have same rank
+    for var, max_values in max_values.items():
         # Use rankdata to rank max values in descending order
         rankings = rankdata([-value for value in max_values], method='min')
         # Convert ranks to integers and store them for the current variable
-        rankings_per_var[var] = [int(rank) for rank in rankings]
+        ranking[var] = [int(rank) for rank in rankings]
+
+    if plot_rank_dist:
+        plot_rankings(ranking, vars, total_member_num)
 
     return
