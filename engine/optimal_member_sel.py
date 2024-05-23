1583cbfae8753cf0c58fd1d6b9ad62a65919ec5c
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2024-05-23 11:53:08 +0200

    Change to random member selection

--- engine/optimal_member_sel.py
+++ engine/optimal_member_sel.py
@@ -2,6 +2,8 @@ import sys
 
 import click
 import numpy as np
+import random
+import os
 
 from engine.check import check_intersection, check_variable
 from engine.tolerance import tolerance
@@ -10,93 +12,28 @@ from util.dataframe_ops import compute_rel_diff_dataframe, parse_probtest_csv
 from util.log_handler import logger
 
 
-def angle_between(A, B):
-    # Convert DataFrame to NumPy array
-    A_array = A.to_numpy()
-    B_array = B.to_numpy()
+def select_members(stats_file_name, member_num, member_type, total_member_num, factor):
 
-    # Find indices where both arrays are not NaN
-    not_nan_indices = ~np.isnan(A_array) & ~np.isnan(B_array)
-    A_array = A_array[not_nan_indices]
-    B_array = B_array[not_nan_indices]
-
-    # Compute the dot product of the flattened arrays of A and B
-    dot_product = np.dot(A_array.flatten(), B_array.flatten())
-
-    # Compute the norms of A and B
-    norm_A = np.linalg.norm(A_array)
-    norm_B = np.linalg.norm(B_array)
-
-    # Compute the angle in radians
-    if norm_A * norm_B == 0:
-        cosine_angle = 1.0  # Return 1.0 when norm_A * norm_B is 0
-    else:
-        cosine_angle = dot_product / (norm_A * norm_B)
-
-    angle_radians = np.arccos(np.clip(cosine_angle, -1.0, 1.0))
-
-    # Convert radians to degrees
-    angle_degrees = np.degrees(angle_radians)
-
-    return angle_degrees
-
-
-def select_members(stats_file_name, member_num, member_type, total_member_num):
-    # read in stats files
-    dfs = [
-        parse_probtest_csv(stats_file_name.format(member_id=m_id), index_col=[0, 1, 2])
-        for m_id in (
-            (str(m_num) if not member_type else member_type + "_" + str(m_num))
-            for m_num in range(1, total_member_num + 1)
+    for i in range(1,101):
+        random_numbers = random.sample(range(1,total_member_num+1),member_num)
+        print('Randomly selected the following members: ', random_numbers)
+        # Create tolerances from random members
+        context = click.Context(tolerance)
+        context.invoke(
+            tolerance,
+            stats_file_name=stats_file_name,
+            tolerance_file_name='random_tolerance.csv',
+            member_num=random_numbers,
+            member_type=member_type,
+            )
+        # Test selection
+        passed = test_selection(
+            stats_file_name, 'random_tolerance.csv', total_member_num, member_type, factor
         )
-    ]
-    ndata = len(dfs)
-    if ndata < 2:
-        logger.critical(
-            (
-                "not enough ensemble members to find optimal spread, got {} dataset."
-                + "Abort."
-            ).format(ndata)
-        )
-        sys.exit(1)
+        if (passed == total_member_num):
+            break
 
-    df_ref = parse_probtest_csv(
-        stats_file_name.format(member_id="ref"), index_col=[0, 1, 2]
-    )
-    dfs_rel = [
-        compute_rel_diff_dataframe(dfs[i], df_ref) for i in range(total_member_num)
-    ]
-    # Only one value per variables and height for each statistic
-    dfs_rel = [r.groupby(["file_ID", "variable"]).max() for r in dfs_rel]
-    # Find first member: maximum norm
-    index = 0
-    value = 0
-    for i in range(total_member_num):
-        norm = 0
-        dfs_rel_array = dfs_rel[i].to_numpy()
-        nan_indices=np.isnan(dfs_rel_array)
-        dfs_rel_array[nan_indices] = 0
-        norm = np.linalg.norm(dfs_rel_array)
-        if norm > value:
-            index = i
-            value = norm
-    indices = np.arange(total_member_num)
-    indices = np.concatenate((indices[:index], indices[index + 1 :]))
-    selection = [index]
-
-    angles = np.zeros(total_member_num)  # selected members will be set to 0
-    for m in range(member_num - 1):
-        for i in indices:
-            angles[i] = angles[i] + angle_between(dfs_rel[selection[-1]], dfs_rel[i])**2
-        max_angle = np.argmax(angles)
-        mask = indices != max_angle
-        angles[max_angle] = 0
-        indices = indices[mask]
-        selection.append(max_angle)
-
-    selection = [x + 1 for x in selection]  # Members start counting at 1
-
-    return selection
+    return random_numbers
 
 
 def test_selection(
@@ -140,7 +77,7 @@ def test_selection(
             passed, total_member_num
         )
     )
-    return
+    return passed
 
 
 @click.command()
@@ -205,18 +142,9 @@ def optimal_member_sel(
         )
     else:
         selection = select_members(
-            stats_file_name, member_num, member_type, total_member_num
+            stats_file_name, member_num, member_type, total_member_num, factor
         )
-        print(selection)
-
-        # Create tolerances from selection
-        context = click.Context(tolerance)
-        context.invoke(
-            tolerance,
-            stats_file_name=stats_file_name,
-            tolerance_file_name=tolerance_file_name,
-            member_num=selection,
-            member_type=member_type,
-        )
+        # The last created file was successful
+        os.rename('random_tolerance.csv',tolerance_file_name)
 
     return
