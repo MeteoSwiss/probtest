4e00b3b1ac842b419440d8868e1cc8444b21147e
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2024-05-24 11:16:14 +0200

    Apply pre-commit

--- engine/optimal_member_sel.py
+++ engine/optimal_member_sel.py
@@ -1,28 +1,28 @@
-import sys
+import os
+import random
+from datetime import datetime
 
 import click
-import numpy as np
-import random
-import os
 
 from engine.check import check_intersection, check_variable
 from engine.tolerance import tolerance
 from util.click_util import CommaSeperatedInts, cli_help
 from util.dataframe_ops import compute_rel_diff_dataframe, parse_probtest_csv
 from util.log_handler import logger
-from datetime import datetime
 
 
 def select_members(stats_file_name, member_num, member_type, total_member_num, factor):
 
-    max_members = 15 # Selection should not have more than 15 members
-    for f in range(int(factor),51): # Try with bigger factor if 15 members are not enough
-        for mem_num in range(member_num,max_members+1):
-            max_passed = 1 # If first 10 random selection have a success rate <60%, try with more
-            for i in range(1,101):
-                random_numbers = random.sample(range(1,total_member_num+1),mem_num)
+    max_members = 15  # Selection should not have more than 15 members
+    for f in range(
+        int(factor), 51
+    ):  # Try with bigger factor if 15 members are not enough
+        for mem_num in range(member_num, max_members + 1):
+            max_passed = 1
+            for i in range(1, 101):
+                random_numbers = random.sample(range(1, total_member_num + 1), mem_num)
                 logger.info(
-                        "Test {} with {} randomly selected members and factor {}.".format(
+                    "Test {} with {} randomly selected members and factor {}.".format(
                         i, mem_num, f
                     )
                 )
@@ -31,38 +31,38 @@ def select_members(stats_file_name, member_num, member_type, total_member_num, f
                 context.invoke(
                     tolerance,
                     stats_file_name=stats_file_name,
-                    tolerance_file_name='random_tolerance.csv',
+                    tolerance_file_name="random_tolerance.csv",
                     member_num=random_numbers,
                     member_type=member_type,
-                    )
+                )
                 # Test selection
                 passed = test_selection(
-                    stats_file_name, 'random_tolerance.csv', total_member_num, member_type, f
+                    stats_file_name,
+                    "random_tolerance.csv",
+                    total_member_num,
+                    member_type,
+                    f,
                 )
 
                 # The following is to save computing time
-                if (i<int(total_member_num/3)):
-                    if (max_passed < passed):
+                if i < int(total_member_num / 3):
+                    if max_passed < passed:
                         max_passed = passed
-                    # The more combs are tested the higher should the success rate be to continue
-                    if (max_passed < i*3):
-                        logger.info(
-                            "Try with more members."
-                            )
+                    # The more combs were tested
+                    # the higher should the success rate be to continue
+                    if max_passed < i * 3:
+                        logger.info("Try with more members.")
                         break
 
-                if (passed == total_member_num):
+                if passed == total_member_num:
                     return random_numbers
-        logger.info(
-            "Increase factor to {}".format(
-                f+1
-            )
-        )
+        logger.info("Increase factor to {}".format(f + 1))
 
     logger.info(
-        "ERROR: Could not find {} random members, which pass for all stat files.".format(
-            max_members
-        )
+        (
+            "ERROR: Could not find {} random members, "
+            + "which pass for all stat files."
+        ).format(max_members)
     )
     exit(1)
 
@@ -71,14 +71,13 @@ def test_selection(
     stats_file_name, tolerance_file_name, total_member_num, member_type, factor
 ):
 
-
     input_file_ref = stats_file_name.format(member_id="ref")
     passed = 0
     df_tol = parse_probtest_csv(tolerance_file_name, index_col=[0, 1])
     df_tol *= factor
     df_ref = parse_probtest_csv(input_file_ref, index_col=[0, 1, 2])
 
-    for m_num in range(1,total_member_num+1):
+    for m_num in range(1, total_member_num + 1):
         m_id = str(m_num) if not member_type else member_type + "_" + str(m_num)
 
         df_cur = parse_probtest_csv(
@@ -179,9 +178,11 @@ def optimal_member_sel(
         end_time = datetime.now()
         elapsed_time = end_time - start_time
         logger.info(
-                "The optimal member selection took {}s.".format(elapsed_time.total_seconds())
+            "The optimal member selection took {}s.".format(
+                elapsed_time.total_seconds()
+            )
         )
         # The last created file was successful
-        os.rename('random_tolerance.csv',tolerance_file_name)
+        os.rename("random_tolerance.csv", tolerance_file_name)
 
-    return
+    return selection
