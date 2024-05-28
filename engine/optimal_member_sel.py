b26a1dc072f6b394d08b813b51003be90dcf0ceb
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2024-05-28 14:38:47 +0200

    Return information about most sensitive variables

--- engine/optimal_member_sel.py
+++ engine/optimal_member_sel.py
@@ -1,6 +1,7 @@
 import os
 import random
 from datetime import datetime
+from collections import Counter
 
 import click
 
@@ -19,8 +20,11 @@ def select_members(stats_file_name, member_num, member_type, total_member_num, f
     for f in range(
         int(factor), 51,5
     ):  # Try with bigger factor if 15 members are not enough
+        logger.info("Set factor to {}".format(f))
         for mem_num in range(member_num, max_members + 1):
+            logger.info("Try with {} members.".format(mem_num))
             max_passed = 1
+            vars = []
             for i in range(1, 51):
                 random_numbers = random.sample(range(1, total_member_num + 1), mem_num)
                 logger.info(
@@ -39,33 +43,38 @@ def select_members(stats_file_name, member_num, member_type, total_member_num, f
                 )
                 # Test selection (not randomly selected members)
                 valid_members = [item for item in members if item not in random_numbers]
-                passed = test_selection(
+                passed, new_vars = test_selection(
                     stats_file_name,
                     "random_tolerance.csv",
                     valid_members,
                     member_type,
                     f,
                 )
+                vars.extend(new_vars)
 
+                duplicates = {item: count for item, count in Counter(vars).items()}
+                sorted_duplicates = dict(sorted(duplicates.items(), key=lambda x: x[1], reverse=True))
                 # The following is to save computing time
-                if i < int((total_member_num-mem_num) / 3):
+                if i < int((total_member_num-mem_num) / 3): # Change to relative number
                     if max_passed < passed:
                         max_passed = passed
                     # The more combs were tested
                     # the higher should the success rate be to continue
-                    if max_passed < i * 3:
-                        logger.info("Try with more members.")
+                    if max_passed < i * 3: # Change to relative number
                         break
 
                 if passed == total_member_num-mem_num:
                     return random_numbers
-        logger.info("Increase factor to {}".format(f + 5))
 
+    max_count = max(sorted_duplicates.values())
+    most_common_vars = [item for item, count in sorted_duplicates.items() if count == max_count]
     logger.info(
         (
-            "ERROR: Could not find {} random members, "
-            + "which pass for all stat files."
-        ).format(max_members)
+            "ERROR: Could not find {} random members, which pass for all stat files. "
+            + "The most sensitive variable(s) is/are {}, which failed for {} out of {} "
+            + "random selections. Consider removing this/those variable(s) from the "
+            + "experiment and run again."
+        ).format(max_members, most_common_vars, max_count, i)
     )
     exit(1)
 
@@ -83,6 +92,7 @@ def test_selection(
     df_tol *= factor
     df_ref = parse_probtest_csv(input_file_ref, index_col=[0, 1, 2])
 
+    vars = []
     for m_num in total_member_num:
         m_id = str(m_num) if not member_type else member_type + "_" + str(m_num)
 
@@ -105,15 +115,21 @@ def test_selection(
 
         out, err, tol = check_variable(diff_df, df_tol)
 
-        if out:
+        if not out:
+            var = set(index[1] for index in err[0].index)
+            var = list(var)
+            vars.extend(var)
+        else:
             passed = passed + 1
+
+    vars = list(set(vars))
 
     logger.info(
         "The tolerance test passed for {} out of {} references.".format(
             passed, len(total_member_num)
         )
     )
-    return passed
+    return passed, vars
 
 
 @click.command()
@@ -196,4 +212,4 @@ def optimal_member_sel(
         # The last created file was successful
         os.rename("random_tolerance.csv", tolerance_file_name)
 
-    return selection
+    return
