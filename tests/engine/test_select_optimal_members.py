37e1e62bdb557c831e55e888ac594c2de4600f2a
Annika Lauber <annika.lauber@c2sm.ethz.ch>
2024-07-04 10:04:47 +0200

    Add actual tests

--- tests/engine/test_select_optimal_members.py
+++ tests/engine/test_select_optimal_members.py
@@ -1,9 +1,15 @@
+import logging
+import os
 import random
+import re
 import unittest
+from io import StringIO
 
 import click
+from click.testing import CliRunner
 
 from engine.select_optimal_members import select_optimal_members
+from engine.tolerance import tolerance
 
 
 def create_dummy_stats_file(filename, configurations, seed, perturbation):
@@ -79,7 +85,7 @@ class TestOptimalMemberSelection(unittest.TestCase):
         # Create stats file with higher perturbation to fail the tolerance test
         create_dummy_stats_file("stats_50.csv", configurations, seed + i, 1e2)
 
-    def test_select(self):
+    def test_select_members(self):
         # Create tolerances from random members
         context = click.Context(select_optimal_members)
         context.invoke(
@@ -88,6 +94,59 @@ class TestOptimalMemberSelection(unittest.TestCase):
             optimal_members_file_name="optimal_members.csv",
             tolerance_file_name="tolerance.csv",
         )
+
+        self.assertTrue(
+            os.path.isfile("optimal_members.csv"),
+            "File 'optimal_members.csv' was not created",
+        )
+
+        with open("optimal_members.csv", "r") as file:
+            content = file.read().strip()
+        expected_content = "50,21,40,39,16\nexport FACTOR=5"
+        self.assertTrue(
+            content == expected_content, "The optimal member selection failed"
+        )
+
+    def test_test_tolerance(self):
+        # Create tolerances out of first five members
+        context = click.Context(tolerance)
+        context.invoke(
+            tolerance,
+            stats_file_name="stats_{member_id}.csv",
+            tolerance_file_name="tolerance.csv",
+            member_num=[5],
+        )
+
+        # Set up logging to capture output with the default format
+        log_stream = StringIO()
+        logging.basicConfig(stream=log_stream, level=logging.INFO, format="%(message)s")
+
+        runner = CliRunner()
+        runner.invoke(
+            select_optimal_members,
+            [
+                "--stats-file-name",
+                "stats_{member_id}.csv",
+                "--optimal-members-file-name",
+                "optimal_members.csv",
+                "--tolerance-file-name",
+                "tolerance.csv",
+                "--test-tolerance",
+            ],
+        )
+
+        # Capture and filter log output
+        log_output = log_stream.getvalue()
+
+        # Extract the number of passed tests from the output message
+        match = re.search(r"passed for (\d+) out of 50", log_output)
+        passed_count = match.group(1)
+        error_message = (
+            f"The test-tolerance output is incorrect. It should pass for "
+            f"49 out of 50 but it passed for {passed_count} out of 50."
+        )
+
+        self.assertTrue("49 out of 50" in log_output, error_message)
 
 
 if __name__ == "__main__":
