b5c28663fe0fef26a4f0b639ef72cb6b5de622b0
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2024-10-04 10:33:43 +0200

    last step

--- setup_env.sh
+++ setup_env.sh
@@ -74,13 +74,12 @@ fi
 ${CONDA} activate ${ENV_NAME}
 
 CONDA_LOC=${CONDA_PREFIX}
-BASE_DIR=$(pwd)
 DEF_PATH_DEFAULT=${CONDA_LOC}/share/eccodes
-DEF_PATH_RESOURCES=${BASE_DIR}/eccodes-cosmo-resources
+DEF_PATH_RESOURCES=${CONDA_LOC}/share/eccodes-cosmo-resources
 
 git clone https://github.com/COSMO-ORG/eccodes-cosmo-resources.git ${DEF_PATH_RESOURCES} || exit
 
-conda env config vars set ECCODES_DEFINITION_PATH=${DEF_PATH_DEFAULT}/definitions:${DEF_PATH_RESOURCES}/definitions
+${CONDA} env config vars set ECCODES_DEFINITION_PATH=${DEF_PATH_DEFAULT}/definitions:${DEF_PATH_RESOURCES}/definitions
 
 echo "Variables saved to environment: "
 ${CONDA} env config vars list
