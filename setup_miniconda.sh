ecfcda568fb475457acdf70e0bf00cf00c25f7b2
stelliom <67868694+stelliom@users.noreply.github.com>
2024-10-08 14:03:10 +0200

    Added missing double quotes for aarch64 comparison

--- setup_miniconda.sh
+++ setup_miniconda.sh
@@ -17,7 +17,7 @@ ARCH="$(arch)"
 MINICONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-py310_23.11.0-1-Linux-${ARCH}.sh
 if [[ ${ARCH} == "x86_64" ]]; then
   SHA256=6581658486be8e700d77e24eccafba586a0fbafafadcf73d35ab13eaee4b80b1
-elif [[ ${ARCH} == aarch64 ]]; then
+elif [[ ${ARCH} == "aarch64" ]]; then
   SHA256=30b3f26fee441c5d70bd50ec06ea1acaa0e373ad30771165eada3f6bdf27766a
 fi
 
