7a02af933582ab0fa03638280012abe890f285de
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2024-12-04 17:35:38 +0100

    test ci

--- setup_env.py
+++ setup_env.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
+""" Setup .env with local eccodes definition """
 
 import os
 import subprocess
-
-# Setup .env with local eccodes definition
 
 base_path = os.getcwd()
 
@@ -11,10 +10,32 @@ eccodes_definition_path = os.path.join(base_path, "resource", "eccodes")
 cosmo_definition_path = os.path.join(base_path, "resource", "eccodes-cosmo-resources")
 
 # Clone repositories
-subprocess.run(["git", "clone", "--depth", "1", "-b", "2.39.0", 
-                "https://github.com/ecmwf/eccodes.git", eccodes_definition_path], check=True)
-subprocess.run(["git", "clone", "--depth", "1", "-b", "v2.36.0.3", 
-                "https://github.com/COSMO-ORG/eccodes-cosmo-resources.git", cosmo_definition_path], check=True)
+subprocess.run(
+    [
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "-b",
+        "2.39.0",
+        "https://github.com/ecmwf/eccodes.git",
+        eccodes_definition_path,
+    ],
+    check=True,
+)
+subprocess.run(
+    [
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "-b",
+        "v2.36.0.3",
+        "https://github.com/COSMO-ORG/eccodes-cosmo-resources.git",
+        cosmo_definition_path,
+    ],
+    check=True,
+)
 
 # Remove deprecated directory
 deprecated_path = os.path.join(eccodes_definition_path, "deprecated")
@@ -27,11 +48,14 @@ if os.path.exists(deprecated_path):
     os.rmdir(deprecated_path)
 
 # Remove existing .env file
-env_file = ".env"
-if os.path.exists(env_file):
-    os.remove(env_file)
+ENV_FILE = ".env"
+if os.path.exists(ENV_FILE):
+    os.remove(ENV_FILE)
 
 # Write to .env file
-env_content = f"ECCODES_DEFINITION_PATH={eccodes_definition_path}/definitions:{cosmo_definition_path}/definitions\n"
-with open(env_file, "w") as file:
-    file.write(env_content)
+ENV_CONTENT = (
+    f"ECCODES_DEFINITION_PATH={eccodes_definition_path}/definitions:"
+    f"{cosmo_definition_path}/definitions\n"
+)
+with open(ENV_FILE, "w", encoding="utf-8") as file:
+    file.write(ENV_CONTENT)
