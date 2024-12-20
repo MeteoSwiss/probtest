4fd11819bcaf3281f30effa5a76f5fbfe1764123
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2024-12-20 11:10:01 +0100

    fix coverage

--- setup_env.py
+++ setup_env.py
@@ -2,60 +2,73 @@
 """ Setup .env with local eccodes definition """
 
 import os
+import shutil
 import subprocess
 
-base_path = os.getcwd()
 
-eccodes_definition_path = os.path.join(base_path, "resource", "eccodes")
-cosmo_definition_path = os.path.join(base_path, "resource", "eccodes-cosmo-resources")
+def main():
+    base_path = os.getcwd()
 
-# Clone repositories
-subprocess.run(
-    [
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "-b",
-        "2.39.0",
-        "https://github.com/ecmwf/eccodes.git",
-        eccodes_definition_path,
-    ],
-    check=True,
-)
-subprocess.run(
-    [
-        "git",
-        "clone",
-        "--depth",
-        "1",
-        "-b",
-        "v2.36.0.3",
-        "https://github.com/COSMO-ORG/eccodes-cosmo-resources.git",
-        cosmo_definition_path,
-    ],
-    check=True,
-)
+    resource_path = os.path.join(base_path, "resource")
 
-# Remove deprecated directory
-deprecated_path = os.path.join(eccodes_definition_path, "deprecated")
-if os.path.exists(deprecated_path):
-    for root, dirs, files in os.walk(deprecated_path, topdown=False):
-        for name in files:
-            os.remove(os.path.join(root, name))
-        for name in dirs:
-            os.rmdir(os.path.join(root, name))
-    os.rmdir(deprecated_path)
+    eccodes_definition_path = os.path.join(resource_path, "eccodes")
+    cosmo_definition_path = os.path.join(resource_path, "eccodes-cosmo-resources")
 
-# Remove existing .env file
-ENV_FILE = ".env"
-if os.path.exists(ENV_FILE):
-    os.remove(ENV_FILE)
+    # remove existing resource
+    if os.path.exists(resource_path):
+        shutil.rmtree(resource_path)
 
-# Write to .env file
-ENV_CONTENT = (
-    f"ECCODES_DEFINITION_PATH={eccodes_definition_path}/definitions:"
-    f"{cosmo_definition_path}/definitions\n"
-)
-with open(ENV_FILE, "w", encoding="utf-8") as file:
-    file.write(ENV_CONTENT)
+    # Clone repositories
+    subprocess.run(
+        [
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            "-b",
+            "2.39.0",
+            "https://github.com/ecmwf/eccodes.git",
+            eccodes_definition_path,
+        ],
+        check=True,
+    )
+    subprocess.run(
+        [
+            "git",
+            "clone",
+            "--depth",
+            "1",
+            "-b",
+            "v2.36.0.3",
+            "https://github.com/COSMO-ORG/eccodes-cosmo-resources.git",
+            cosmo_definition_path,
+        ],
+        check=True,
+    )
+
+    # Remove deprecated directory
+    deprecated_path = os.path.join(eccodes_definition_path, "deprecated")
+    if os.path.exists(deprecated_path):
+        for root, dirs, files in os.walk(deprecated_path, topdown=False):
+            for name in files:
+                os.remove(os.path.join(root, name))
+            for name in dirs:
+                os.rmdir(os.path.join(root, name))
+        os.rmdir(deprecated_path)
+
+    # Remove existing .env file
+    env_file = ".env"
+    if os.path.exists(env_file):
+        os.remove(env_file)
+
+    # Write to .env file
+    env_content = (
+        f"ECCODES_DEFINITION_PATH={eccodes_definition_path}/definitions:"
+        f"{cosmo_definition_path}/definitions\n"
+    )
+    with open(env_file, "w", encoding="utf-8") as file:
+        file.write(env_content)
+
+
+if __name__ == "__main__":
+    main()
