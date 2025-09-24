037987fe5e64b455b465d291cf51e27a1e9c2acb
Chiara Ghielmini <chiara.ghielmini@meteoswiss.ch>
2025-09-24 14:52:23 +0200

    implemented verison of check fof files

--- engine/check_fof.py
+++ engine/check_fof.py
@@ -5,92 +5,81 @@ This module provides a command-line interface (CLI) to check that
 two given fof files are conistent.
 """
 
-import hashlib
-
 import click
 import numpy as np
 import xarray as xr
-import os
-
-# variable that are not supposed to change across fof files
-hash_vars = [
-    "lat",
-    "lon",
-    "statid",
-    "data_category",
-    "sub_category",
-    "obstype",
-    "d_body",
-    "d_veri",
-    "d_2",
-    "d_3",
-    "l_body",
-    "n_level",
-    "center",
-    "sub_center",
-    "codetype",
-    "ident",
-    "time",
-    "time_nomi",
-    # "time_dbase",
-    "z_station",
-    "z_modsurf",
-    "sta_corr",
-    "index_x",
-    "index_y",
-    "mdlsfc",
-    "instype",
-    "obs_id",
-    #  "source",
-    "subset",
-    "dbkz",
-    "index_d",
-    # "varno",
-    "bcor",
-    "level_typ",
-    # "qual",
-    "veri_model",
-    "veri_run_type",
-    "veri_run_class",
-    "veri_initial_date",
-    "veri_forecast_time",
-    "veri_resolution",
-    "veri_domain_size",
-    "veri_description",
-    "veri_ens_member",
-    "veri_exp_id",
-    "veri_operator_flag",
-    "n_hdr",
-    "n_body",
-    "n_radar",
-    "plevel",
-]
 
 
-def compute_hash_for_vars_and_attrs(ds):
+def get_dims(ds):
+    """
+    Get dimension of reports and observations
+    """
+    vars_shape1 = []
+    vars_shape2 = []
+    shape1 = ds.attrs["n_hdr"]
+    shape2 = ds.attrs["n_body"]
 
-    all_bytes = b""
+    for var in ds.data_vars:
+        if ds[var].shape[0] == shape1:
+            vars_shape1.append(var)
+        elif ds[var].shape[0] == shape2:
+            vars_shape2.append(var)
 
-    for var in hash_vars:
-        if var in ds.data_vars:
-            var_data = ds[var].values
-            all_bytes += np.sort(var_data.ravel()).tobytes()
+    return vars_shape1, vars_shape2
 
-            for attr, value in sorted(ds[var].attrs.items()):
-                all_bytes += str(attr).encode("utf-8") + str(value).encode("utf-8")
 
-    return hashlib.md5(all_bytes).hexdigest()
+def split_feedback_dataset(ds):
+    """
+    Split feedback file according to reports and oservations dimensions,
+    expand lat, lon, statid and time_nomi accoridng to l_body
+    and sort them to assure unique order
+    """
+    vars_shape1, _ = get_dims(ds)
+    ds_reports = ds[vars_shape1]
+
+    sort_keys_reports = ["lat", "lon", "statid", "time_nomi", "codetype"]
+    ds_report_sorted = ds_reports.sortby(sort_keys_reports)
+
+    coords = ds.coords.keys()
+
+    dataset = ds
+    lbody = ds["l_body"].values
+
+    for varname in filter(
+        lambda s: s in ["lat", "lon", "statid", "time_nomi"], dataset
+    ):
+        values = np.repeat(dataset[varname], lbody.astype(int))
+        attrs = dataset[varname].attrs
+        dataset = dataset.assign({varname: xr.Variable("d_body", values, attrs=attrs)})
+
+    dataset = dataset.assign_coords(
+        dict(zip(coords, [dataset[coord] for coord in coords]))
+    )
+
+    _, vars_shape2 = get_dims(dataset)
+
+    ds_obs = dataset[vars_shape2]
+    sort_keys_obs = ["lat", "lon", "statid", "varno", "level", "time_nomi"]
+    ds_obs_sorted = ds_obs.sortby(sort_keys_obs)
+
+    return ds_report_sorted, ds_obs_sorted
 
 
 def compare_arrays(arr1, arr2, var_name):
+    """
+    Comparison of two arrays containing the values of the same variable.
+    If not the same, it tells you in percentage terms how different they are.
+    """
     total = arr1.size
 
     if np.array_equal(arr1, arr2):
         equal = total
 
-    elif (np.issubdtype(arr1.dtype, np.number)
-          and np.issubdtype(arr2.dtype, np.number)
-          and np.array_equal(arr1, arr2, equal_nan=True)):
+    elif (
+        np.issubdtype(arr1.dtype, np.number)
+        and np.issubdtype(arr2.dtype, np.number)
+        and np.array_equal(arr1, arr2, equal_nan=True)
+    ):
         equal = total
 
     else:
@@ -105,85 +94,74 @@ def compare_arrays(arr1, arr2, var_name):
     return total, equal
 
 
-def compare_nc_files(file1, file2):
-    ds1 = xr.open_dataset(file1)
-    ds2 = xr.open_dataset(file2)
-
-    hash1 = compute_hash_for_vars_and_attrs(ds1)
-    hash2 = compute_hash_for_vars_and_attrs(ds2)
-
-    if hash1 == hash2:
-        print("fof files are consistent")
-
-        return
-
-    print("fof files are NOT consistent")
-    if os.path.basename(file1) != os.path.basename(file2):
-        return
-    #print(os.path.basename(file1))
-
-    index_vars = ["lat", "lon", "statid"]
-    total_elements_all = 0
-    equal_elements_all = 0
-
-    ds1_sorted = ds1.sortby(index_vars)
-    ds2_sorted = ds2.sortby(index_vars)
-
-    for var in hash_vars:
-        # variable comparison
-        if var in ds1_sorted.data_vars and var in ds2_sorted.data_vars:
-            arr1 = ds1_sorted[var].values
-            arr2 = ds2_sorted[var].values
-
-            if arr1.size == arr2.size:
-                total, equal = compare_arrays(arr1, arr2, var)
-                total_elements_all += total
-                equal_elements_all += equal
-            else:
-
-                total_elements_all += max(arr1.size, arr2.size)
-                equal_elements_all += 0
+def prepare_array(arr):
+    """
+    To make sure nan values are recognised.
+    """
+    if arr.dtype == np.float32 and np.isnan(arr).any():
+        return np.where(np.isnan(arr), -999999, arr)
+    return arr
 
 
-        # attribute comparison
-        if var in ds1_sorted.attrs and var in ds2_sorted.attrs:
-            arr1 = np.array(ds1_sorted.attrs[var], dtype=object)
-            arr2 = np.array(ds2_sorted.attrs[var], dtype=object)
+def compare_var_and_attr_ds(ds1, ds2):
+    """
+    Variable by variable and attribute by attribute,
+    comparison of the two files.
+    """
+
+    total_all, equal_all = 0, 0
+
+    for var in set(ds1.data_vars).union(ds2.data_vars):
+        if var in ds1.data_vars and var in ds2.data_vars:
+            arr1 = prepare_array(ds1[var].values)
+            arr2 = prepare_array(ds2[var].values)
+
             if arr1.size == arr2.size:
                 t, e = compare_arrays(arr1, arr2, var)
-                total_elements_all += t
-                equal_elements_all += e
-
             else:
-                total_elements_all += max(arr1.size, arr2.size)
-                equal_elements_all += 0
+                t, e = max(arr1.size, arr2.size), 0
 
-    if total_elements_all > 0:
-        percent_equal_all = (equal_elements_all / total_elements_all) * 100
-        percent_diff_all = 100 - percent_equal_all
-        print(f"Total percentage of equality: {percent_equal_all:.2f}%")
-        print(f"Total percentage of difference: {percent_diff_all:.2f}%")
+            total_all += t
+            equal_all += e
+
+        if var in ds1.attrs and var in ds2.attrs:
+            arr1 = np.array(ds1.attrs[var], dtype=object)
+            arr2 = np.array(ds2.attrs[var], dtype=object)
+            if arr1.size == arr2.size:
+                t, e = compare_arrays(arr1, arr2, var)
+            else:
+                t, e = max(arr1.size, arr2.size), 0
+            total_all += t
+            equal_all += e
+
+    return total_all, equal_all
 
 
 @click.command()
 @click.argument("file1", type=click.Path(exists=True))
 @click.argument("file2", type=click.Path(exists=True))
 def check_fof(file1, file2):
+    ds1 = xr.open_dataset(file1)
+    ds2 = xr.open_dataset(file2)
 
-    if "SYNOP" in file1:
-        ad = ["phase"]
-        hash_vars.extend(ad)
-    elif "AIREP" in file1:
-        ad = ["tracking", "phase"]
-        hash_vars.extend(ad)
-    elif "PILOT" in file1:
-        ad = ["tracking", "meas_type"]
-        hash_vars.extend(ad)
-    elif "TEMP" in file1:
-        ad = ["tracking", "rad_corr", "meas_type", "dlat", "dlon"]
-        hash_vars.extend(ad)
+    ds_reports1_sorted, ds_obs1_sorted = split_feedback_dataset(ds1)
+    ds_reports2_sorted, ds_obs2_sorted = split_feedback_dataset(ds2)
 
-    compare_nc_files(file1, file2)
+    total_elements_all, equal_elements_all = 0, 0
+
+    for ds1, ds2 in [
+        (ds_reports1_sorted, ds_reports2_sorted),
+        (ds_obs1_sorted, ds_obs2_sorted),
+    ]:
+        t, e = compare_var_and_attr_ds(ds1, ds2)
+        total_elements_all += t
+        equal_elements_all += e
+
+    if total_elements_all > 0:
+        percent_equal_all = (equal_elements_all / total_elements_all) * 100
+        percent_diff_all = 100 - percent_equal_all
+        print(f"Total percentage of equality: {percent_equal_all:.2f}%")
+        print(f"Total percentage of difference: {percent_diff_all:.2f}%")
 
 
 if __name__ == "__main__":
