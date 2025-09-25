54d7000c683334ec6e95e6c4ebbb8c9f735ed7f5
Chiara Ghielmini <chiara.ghielmini@meteoswiss.ch>
2025-09-25 10:32:31 +0200

    Add option to print entire line in case of differences

--- engine/check_fof.py
+++ engine/check_fof.py
@@ -31,7 +31,7 @@ def get_dims(ds):
 def split_feedback_dataset(ds):
     """
     Split feedback file according to reports and oservations dimensions,
-    expand lat, lon, statid and time_nomi accoridng to l_body
+    expand lat, lon, statid and time_nomi according to l_body
     and sort them to assure unique order
     """
     vars_shape1, _ = get_dims(ds)
@@ -74,6 +74,7 @@ def compare_arrays(arr1, arr2, var_name):
 
     if np.array_equal(arr1, arr2):
         equal = total
+        diff = np.array([])
 
     elif (
         np.issubdtype(arr1.dtype, np.number)
@@ -81,6 +82,7 @@ def compare_arrays(arr1, arr2, var_name):
         and np.array_equal(arr1, arr2, equal_nan=True)
     ):
         equal = total
+        diff = np.array([])
 
     else:
         mask_equal = arr1 == arr2
@@ -88,10 +90,12 @@ def compare_arrays(arr1, arr2, var_name):
         percent = (equal / total) * 100
         print(f"Differences in '{var_name}': {percent:.2f}% equal")
         diff_idx = np.where(~mask_equal.ravel())[0]
-        for i in diff_idx[:20]:
+
+        diff = diff_idx[:20]
+        for i in diff:
             print(f"   - ds1: {arr1.ravel()[i]}, ds2: {arr2.ravel()[i]}")
 
-    return total, equal
+    return total, equal, diff
 
 
 def prepare_array(arr):
@@ -103,7 +107,25 @@ def prepare_array(arr):
     return arr
 
 
-def compare_var_and_attr_ds(ds1, ds2):
+def print_entire_line(ds1, ds2, diff):
+    """
+    If the specific option is called, this function print
+    the entire line in which differences were found
+    """
+    if diff.size > 0:
+        da1 = ds1.to_dataframe().reset_index()
+        da2 = ds2.to_dataframe().reset_index()
+
+        for i in diff:
+            row1 = " | ".join(map(str, da1.loc[i]))
+            row2 = " | ".join(map(str, da2.loc[i]))
+
+            print(f"ds1: {row1}")
+            print(f"ds2: {row2}")
+            print("-" * max(len(row1), len(row2)))
+
+
+def compare_var_and_attr_ds(ds1, ds2, entireline):
     """
     Variable by variable and attribute by attribute,
     comparison of the two files.
@@ -117,7 +139,10 @@ def compare_var_and_attr_ds(ds1, ds2):
             arr2 = prepare_array(ds2[var].values)
 
             if arr1.size == arr2.size:
-                t, e = compare_arrays(arr1, arr2, var)
+                t, e, diff = compare_arrays(arr1, arr2, var)
+                if entireline:
+                    print_entire_line(ds1, ds2, diff)
+
             else:
                 t, e = max(arr1.size, arr2.size), 0
 
@@ -128,7 +153,10 @@ def compare_var_and_attr_ds(ds1, ds2):
             arr1 = np.array(ds1.attrs[var], dtype=object)
             arr2 = np.array(ds2.attrs[var], dtype=object)
             if arr1.size == arr2.size:
-                t, e = compare_arrays(arr1, arr2, var)
+                t, e, diff = compare_arrays(arr1, arr2, var)
+
+                if entireline:
+                    print_entire_line(ds1, ds2, diff)
             else:
                 t, e = max(arr1.size, arr2.size), 0
             total_all += t
@@ -140,7 +168,10 @@ def compare_var_and_attr_ds(ds1, ds2):
 @click.command()
 @click.argument("file1", type=click.Path(exists=True))
 @click.argument("file2", type=click.Path(exists=True))
-def check_fof(file1, file2):
+@click.option(
+    "--entireline", is_flag=True, help="If there are differences, printe the whole line"
+)
+def check_fof(file1, file2, entireline):
     ds1 = xr.open_dataset(file1)
     ds2 = xr.open_dataset(file2)
 
@@ -153,7 +184,7 @@ def check_fof(file1, file2):
         (ds_reports1_sorted, ds_reports2_sorted),
         (ds_obs1_sorted, ds_obs2_sorted),
     ]:
-        t, e = compare_var_and_attr_ds(ds1, ds2)
+        t, e = compare_var_and_attr_ds(ds1, ds2, entireline)
         total_elements_all += t
         equal_elements_all += e
 
