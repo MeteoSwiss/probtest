82fa08790030a7ae517f8ce2854ab303a296ada6
Daniel Hupp <daniel.hupp@meteoswiss.ch>
2025-10-16 09:45:51 +0200

    fix formating

--- tests/cli/test_tolerance_cli.py
+++ tests/cli/test_tolerance_cli.py
@@ -6,6 +6,7 @@ and check for discrepancies.
 """
 
 import os
+
 import pytest
 
 from tests.helpers import (
@@ -18,12 +19,19 @@ from tests.helpers import (
 
 
 @pytest.mark.parametrize("use_minimum_tolerance", [True, False])
-def test_tolerance_cli(ref_data, df_ref_tolerance, tmp_dir, new_ref, use_minimum_tolerance):
+def test_tolerance_cli(
+    ref_data, df_ref_tolerance, tmp_dir, new_ref, use_minimum_tolerance
+):
     stats_file_name = os.path.join(ref_data, "stats_{member_id}.csv")
     tolerance_file_name = os.path.join(tmp_dir, "tolerance.csv")
 
     if use_minimum_tolerance:
-        run_tolerance_cli(stats_file_name, tolerance_file_name, member_type="dp", minimum_tolerance=1e-14)
+        run_tolerance_cli(
+            stats_file_name,
+            tolerance_file_name,
+            member_type="dp",
+            minimum_tolerance=1e-14,
+        )
         df_test = load_pandas(tolerance_file_name, index_col=[0, 1])
         err = pandas_error(df_ref_tolerance, df_test)
     else:
@@ -33,9 +41,9 @@ def test_tolerance_cli(ref_data, df_ref_tolerance, tmp_dir, new_ref, use_minimum
         df_test = load_pandas(tolerance_file_name, index_col=[0, 1])
         modified_df_ref_tolerance = df_ref_tolerance
         modified_df_ref_tolerance.loc[("NetCDF:*.nc", "T")] = 0.0
-        
+
         err = pandas_error(modified_df_ref_tolerance, df_test)
-    
+
     store_as_potential_new_ref(tolerance_file_name, new_ref)
 
     assert_empty_df(err, "Tolerance datasets are not equal!")
