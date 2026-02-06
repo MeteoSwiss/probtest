89f69b41cab4c50c1cf916c47e086ae945fc6162
cghielmini <chiara.ghielmini@meteoswiss.ch>
2026-02-06 13:58:25 +0100

    Fix check_plot (#90)
    This pull request fixes a minor bug in the check_plot code introduced by
    changes in the latest version of probtest and adds a test to prevent
    other bugs from recurring in the future.

--- tests/conftest.py
+++ tests/conftest.py
@@ -360,3 +360,56 @@ def fof_file_set(tmp_dir, sample_dataset_fof):
     for path in files["fof"] + files["tol"] + [files["members"]]:
         if os.path.exists(path):
             os.remove(path)
+
+
+@pytest.fixture(name="stats_dataframes", scope="function")
+def fixture_dataframes(tmp_dir):
+    """
+    Create stats dataframes and reference tolerances.
+    """
+
+    file_ids = ["NetCDF:*atm_3d*.nc"]
+    variables = ["var_1", "var_2"]
+    times = range(4)
+    stats = ["max", "mean", "min"]
+
+    index = pd.MultiIndex.from_arrays(
+        [file_ids * 4, ["var_1"] * 3 + ["var_2"], list(range(3)) + [0]],
+        names=["file_ID", "variable", "height"],
+    )
+    columns = pd.MultiIndex.from_product([times, stats], names=["time", "statistic"])
+
+    array1 = np.linspace(0.9, 1.9, 4 * 12).reshape(4, 12)
+    array1[:2] *= -1
+    df1 = pd.DataFrame(array1, index=index, columns=columns)
+
+    array2 = np.linspace(1.1, 2.1, 4 * 12).reshape(4, 12)
+    array2[:2] *= -1
+    df2 = pd.DataFrame(array2, index=index, columns=columns)
+
+    base_index = pd.Index(variables, name="variable")
+    tol_large_base = pd.DataFrame(
+        np.ones((2, 12)) * 0.21, index=base_index, columns=columns
+    )
+    tol_small_base = pd.DataFrame(
+        np.ones((2, 12)) * 0.06, index=base_index, columns=columns
+    )
+
+    tol_large = pd.concat(
+        [tol_large_base] * len(file_ids), keys=file_ids, names=["file_ID", "variable"]
+    )
+    tol_small = pd.concat(
+        [tol_small_base] * len(file_ids), keys=file_ids, names=["file_ID", "variable"]
+    )
+
+    df1_stats = os.path.join(tmp_dir, "stats1.csv")
+    df2_stats = os.path.join(tmp_dir, "stats2.csv")
+    tol_large_stats = os.path.join(tmp_dir, "tol_large.csv")
+    tol_small_stats = os.path.join(tmp_dir, "tol_small.csv")
+
+    df1.to_csv(df1_stats)
+    df2.to_csv(df2_stats)
+    tol_large.to_csv(tol_large_stats)
+    tol_small.to_csv(tol_small_stats)
+
+    yield df1_stats, df2_stats, tol_large_stats, tol_small_stats
