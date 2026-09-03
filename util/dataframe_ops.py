2d366fbff77a798df1aa0c60ec2cef55ec396a8a
alitzel-macias <71673990+alitzel-macias@users.noreply.github.com>
2026-09-03 17:12:30 +0400

    Fail when reference has different variables (#118)
    In the `check` task, when the current run has extra variables that are
    not in the reference files then the probtest check does not fail but
    instead just throws a warning: "UserWarning: WARNING: The following
    variables are in the test case but not in the reference case and
    therefore not tested: new_var". Because this is only a warning, then the
    Säntis CI pipeline is shown as success (green check) instead of showing
    also a warning or failing. This is a problem because the user doesn't
    know about the warning unless entering the log output.
    This PR changes the behaviour so that when the variables are different
    than the references' then the test is failed.

--- util/dataframe_ops.py
+++ util/dataframe_ops.py
@@ -219,12 +219,12 @@ def unify_time_index(fid_dfs):
 def check_intersection(df_ref, df_cur):
     # Check if variable names in reference and test case have any intersection
     # Check if numbers of time steps agree
-    skip_test = 0
+    skip_test = False
     if not set(df_ref.index.intersection(df_cur.index)):
         logger.info(
             "WARNING: No intersection between variables in input and reference file."
         )
-        skip_test = 1
+        skip_test = True
         return skip_test, df_ref, df_cur
 
     # Check if there are variable missing in the reference or test case
@@ -245,12 +245,14 @@ def check_intersection(df_ref, df_cur):
             f" reference case and therefore not tested: {', '.join(missing_in_ref)}"
         )
         warnings.warn(warning_msg, UserWarning)
+        skip_test = True
     if missing_in_cur:
         warning_msg = (
             "WARNING: The following variables are in the reference case but not in the"
             f" test case and therefore not tested: {', '.join(missing_in_cur)}"
         )
         warnings.warn(warning_msg, UserWarning)
+        skip_test = True
 
     # Remove rows without intersection
     df_ref = df_ref[~df_ref.index.isin(non_common_vars)]
@@ -370,7 +372,7 @@ def check_file_with_tolerances(
         # check if variables are available in reference file
         skip_test, df_ref, df_cur = check_intersection(df_ref, df_cur)
 
-        if skip_test:  # No intersection
+        if skip_test:  # No intersection or mismatched variable sets
             logger.error("RESULT: check FAILED")
             sys.exit(1)
 
